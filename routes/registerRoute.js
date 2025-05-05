2083f5df82942d72fc7c68fd3b2444ac918ff595
mafimashrafi <mafimashrafi78@gmail.com>
2025-05-05 10:35:58 +0600

    again more bugs

--- routes/registerRoute.js
+++ routes/registerRoute.js
@@ -43,22 +43,19 @@ router.post("/register", async (req, res) => {
         email: email,
         password: hashPassword,
         phoneNumber: phoneNumber,
-        isVerified: false, // Initially set as not verified
+        isVerified: false,
         createdAt: new Date(),
     });
 
     newUser.save()
         .then(async (user) => {
-            console.log("User created successfully");
 
-            // Generate a verification token
             const vtoken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
 
-            // Send the verification email
             const verificationLink = `${process.env.BASE_URL}/verify-email?vtoken=${vtoken}`;
             await sendVerificationEmail(user.email, verificationLink);
 
-            res.render("register.ejs", { error: "Verification email sent. Please check your inbox." });
+            res.render("register.ejs", { error: "Verification email sent. Please check your inbox/spam." });
         })
         .catch((err) => {
             console.log(err);
